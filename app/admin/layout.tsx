17ad92c6fe496c35141fad1836c8550276fe7aef
Ephrain Thayil <ethayil@gmail.com>
2026-06-06 21:28:12 +0530

    add admin layout

---
+++ app/admin/layout.tsx
@@ -0,0 +1,31 @@
+import { redirect } from "next/navigation";
+import { GridBackgroundPattern } from "@/components/ui/text-reveal-effects";
+import { getSession } from "@/lib/get-session";
+
+export default async function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { user } = await getSession();
+  if (!user) {
+    return redirect("/auth/signin");
+  }
+
+  if (!user) {
+    redirect("/auth/signin");
+  }
+
+  if (user.role !== "superAdmin") {
+    redirect("/verify");
+  }
+
+  return (
+    <div className="min-h-svh bg-background text-foreground relative flex flex-col items-center justify-center p-4 overflow-hidden">
+      <GridBackgroundPattern />
+      <div className="z-10 w-full max-w-4xl animate-in fade-in slide-in-from-bottom-4 duration-700 ease-out">
+        {children}
+      </div>
+    </div>
+  );
+}
