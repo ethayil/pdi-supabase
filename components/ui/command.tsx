b258a33b3e05995447b599f3c39e751954deeff3
Ephrain Thayil <ethayil@gmail.com>
2026-06-09 18:20:23 +0530

    fix commad pallet dialog showing at the bottom of the page

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -51,7 +51,7 @@ function CommandDialog({
       </DialogHeader>
       <DialogContent
         className={cn(
-          "top-1/3 translate-y-0 overflow-hidden rounded-xl! p-0 bg-surface-gradient backdrop-blur-surface",
+          "overflow-hidden rounded-xl! p-0 bg-surface-gradient backdrop-blur-surface",
           className,
         )}
         showCloseButton={showCloseButton}
